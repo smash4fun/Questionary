91a719e4ea6a88a2c57581141adf5d7b246aee16
smash4fun <88120551+smash4fun@users.noreply.github.com>
2022-01-02 12:49:23 -0600

    Add files via upload

---
+++ PRO-C36--Template-main/PRO-C36--Template-main/question.js
@@ -0,0 +1,3 @@
+this.question("Question:- What starts and ends with the letter'E', but has only one letter?");
+this.message.html("Thank You, Your Answer has Been Submitted")
+this.Message.position(350,350)
